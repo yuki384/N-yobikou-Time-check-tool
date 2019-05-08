b6d3716b56530204529d02a59c1e31c23dbb8dd5
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2019-05-08 19:52:41 +0900

    完成！！！！

--- content.js
+++ content.js
@@ -4,12 +4,15 @@ var time = 0;
 for (var i=0; i<times.length; i++){
   time = Number(times[i].innerText.substr(0,2))*60+Number(times[i].innerText.substr(-2))+time;
 }
-console.log("合計"+Math.floor(time/3600)+"時間"+Math.floor((time%3600)/60)+"分"+(time%60)+"秒");
+var goukei = "すべての授業: "+Math.floor(time/3600)+"時間"+Math.floor((time%3600)/60)+"分"+(time%60)+"秒";
 
 var goodtime=0;
 for (var i=0; i<goodtimes.length; i++){
   goodtime = Number(goodtimes[i].innerText.substr(0,2))*60+Number(goodtimes[i].innerText.substr(-2))+goodtime;
 }
-console.log("見終わった授業:"+Math.floor(goodtime/3600)+"時間"+Math.floor((goodtime%3600)/60)+"分"+(goodtime%60)+"秒");
+var goodgoukei = "視聴済み: "+Math.floor(goodtime/3600)+"時間"+Math.floor((goodtime%3600)/60)+"分"+(goodtime%60)+"秒";
 
-console.log("残りの授業時間:"+Math.floor((time-goodtime)/3600)+"時間"+Math.floor(((time-goodtime)%3600)/60)+"分"+((time-goodtime)%60)+"秒");
+var remaininggoukei="未視聴: "+Math.floor((time-goodtime)/3600)+"時間"+Math.floor(((time-goodtime)%3600)/60)+"分"+((time-goodtime)%60)+"秒";
+
+var hyouji = document.getElementsByClassName('description');
+hyouji[0].innerHTML="<div class='u-card'><div class='u-list-header typo-list-title'>この単元の進捗状況</div><div class='u-card-inner'>"+goukei+"<br>"+goodgoukei+"<br>"+remaininggoukei+"</div></div>"+hyouji[0].innerHTML;
