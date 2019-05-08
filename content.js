a3b3984c6e408140a1b56f5f0c2017fa383adc1a
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2019-05-08 23:01:07 +0900

    いい感じにしたやつをいい感じに（インデント）してみたり？

--- content.js
+++ content.js
@@ -7,7 +7,7 @@ Number.prototype.secToTime = function() {
 }
 
 String.prototype.toSec = function() {
-  return this.split(':').reduce((x, y) => x * 60 + (y >> 0));
+	return this.split(':').reduce((x, y) => x * 60 + (y >> 0));
 }
 
 Array.prototype.sum = function() {
@@ -18,36 +18,36 @@ Array.prototype.sum = function() {
 const time = {
 	total: null,
 	end: null,
-  
-  update: () => {
-    time.total = [...document.getElementsByClassName('movie-length')].map(dom => dom.innerText.toSec()).sum();
-    time.end = [...document.getElementsByClassName('good')].map(dom => dom.innerText.match(/(\d+:\d+)$/)[0].toSec()).sum();
-  }
+
+	update: () => {
+		time.total = [...document.getElementsByClassName('movie-length')].map(dom => dom.innerText.toSec()).sum();
+		time.end = [...document.getElementsByClassName('good')].map(dom => dom.innerText.match(/(\d+:\d+)$/)[0].toSec()).sum();
+	}
 };
 
 const output = document.getElementsByClassName('description')[0];
 const description = output.innerHTML;
 
 const display = () => {
-  output.innerHTML = `
-    <div class='u-card'>
-      <div class='u-list-header typo-list-title'>この単元の進捗状況</div>
-        <div class='u-card-inner'>
-          <p>合計時間 : ${time.total.secToTime()}</p>
-          <p>終了時間 : ${time.end.secToTime()}</p>
-          <p>残り時間 : ${(time.total - time.end).secToTime()}</p>
-        </div>
-      </div>
-    </div>
-    ${description}
+	output.innerHTML = `
+		<div class='u-card'>
+			<div class='u-list-header typo-list-title'>この単元の進捗状況</div>
+				<div class='u-card-inner'>
+					<p>合計時間 : ${time.total.secToTime()}</p>
+					<p>終了時間 : ${time.end.secToTime()}</p>
+					<p>残り時間 : ${(time.total - time.end).secToTime()}</p>
+				</div>
+			</div>
+		</div>
+		${description}
 	`;
 };
 
 const main = () => {
-  time.update();
-  display();
+	time.update();
+	display();
 
-  requestAnimationFrame(main);
+	requestAnimationFrame(main);
 };
 
 main();
