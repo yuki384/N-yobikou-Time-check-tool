2bb7e47a6f5fe76c8be4e67e40cb5218b2e301b0
Yoshihisa Kaino <yoppy1125yoppy@gmail.com>
2019-05-09 08:05:42 +0900

    Update replace var with let

--- content.js
+++ content.js
@@ -1,18 +1,18 @@
-var times = document.getElementsByClassName('movie-length');
-var goodtimes = document.querySelectorAll(".good .movie-length");
-var time = 0;
-for (var i=0; i<times.length; i++){
+let times = document.getElementsByClassName('movie-length');
+let goodtimes = document.querySelectorAll(".good .movie-length");
+let time = 0;
+let (let i=0; i<times.length; i++){
   time = Number(times[i].innerText.substr(0,2))*60+Number(times[i].innerText.substr(-2))+time;
 }
-var goukei = "すべての授業: "+Math.floor(time/3600)+"時間"+Math.floor((time%3600)/60)+"分"+(time%60)+"秒";
+let goukei = "すべての授業: "+Math.floor(time/3600)+"時間"+Math.floor((time%3600)/60)+"分"+(time%60)+"秒";
 
-var goodtime=0;
-for (var i=0; i<goodtimes.length; i++){
+let goodtime=0;
+for (let i=0; i<goodtimes.length; i++){
   goodtime = Number(goodtimes[i].innerText.substr(0,2))*60+Number(goodtimes[i].innerText.substr(-2))+goodtime;
 }
-var goodgoukei = "視聴済み: "+Math.floor(goodtime/3600)+"時間"+Math.floor((goodtime%3600)/60)+"分"+(goodtime%60)+"秒";
+let goodgoukei = "視聴済み: "+Math.floor(goodtime/3600)+"時間"+Math.floor((goodtime%3600)/60)+"分"+(goodtime%60)+"秒";
 
-var remaininggoukei="未視聴: "+Math.floor((time-goodtime)/3600)+"時間"+Math.floor(((time-goodtime)%3600)/60)+"分"+((time-goodtime)%60)+"秒";
+let remaininggoukei="未視聴: "+Math.floor((time-goodtime)/3600)+"時間"+Math.floor(((time-goodtime)%3600)/60)+"分"+((time-goodtime)%60)+"秒";
 
-var hyouji = document.getElementsByClassName('description');
+let hyouji = document.getElementsByClassName('description');
 hyouji[0].innerHTML="<div class='u-card'><div class='u-list-header typo-list-title'>この単元の進捗状況</div><div class='u-card-inner'>"+goukei+"<br>"+goodgoukei+"<br>"+remaininggoukei+"</div></div>"+hyouji[0].innerHTML;
