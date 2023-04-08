8e5d9b3e1bc761a2fa44c2b87be1b00d9f89de25
mono0218 <81796635+mono0218@users.noreply.github.com>
2023-04-09 07:12:55 +0900

    support latest version

--- content.js
+++ content.js
@@ -2,8 +2,10 @@
 let times = document.getElementsByClassName('movie-length');
 let goodtimes = document.querySelectorAll(".good .movie-length");
 let time = 0;
+
 for (let i=0; i<times.length; i++){
-  time = Number(times[i].innerText.substr(0,2))*60+Number(times[i].innerText.substr(-2))+time;
+  const c1 = times[i].innerText.split(":");
+  time = Number(c1[0])*60+Number(c1[1])+time;
   //times変数（mm:ssの文字列で渡される）を秒数の数値に変換して合計
 }
 
@@ -11,8 +13,8 @@ let goukei = "すべての授業: "+Math.floor(time/3600)+"時間"+Math.floor((t
 
 let goodtime=0;
 for (let i=0; i<goodtimes.length; i++){
-  goodtime = Number(goodtimes[i].innerText.substr(0,2))*60+Number(goodtimes[i].innerText.substr(-2))+goodtime;
-  //time変数と同様、goodtimesに入ってる文字列を数値に変換
+  const c1 = goodtimes[i].innerText.split(":");
+  goodtime = Number(c1[0])*60+Number(c1[1])+goodtime;
 }
 
 let goodgoukei = "視聴済み: "+Math.floor(goodtime/3600)+"時間"+Math.floor((goodtime%3600)/60)+"分"+(goodtime%60)+"秒（" + Math.round((goodtime / time) * 100) + "%）";
@@ -30,3 +32,4 @@ let testCount = document.getElementsByClassName('evaluation-test').length;
 
 hyouji[0].innerHTML="<div class='u-card'><div class='u-list-header typo-list-title'>この単元の進捗状況</div><div class='u-card-inner'>"+goukei+"<br>"+goodgoukei+"<br>"+remaininggoukei + "<br>" + "授業動画数:" + movieCount + "<br>" + "確認テストの数:" + testCount + "</div></div>"+hyouji[0].innerHTML;
 //表示
+
